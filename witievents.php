cb94a4025e69f12bbaed9eebba395ada93e62388
Richard Hogge <richard@tremolocreative.com>
2020-09-01 22:06:33 -0700

    initial commit

---
+++ witievents.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WITI Events
+Plugin URI: https://witi.com
+Description: Upcoming events feed widget for WITI (Women in Technology International)
+Version: 1.0.0
+Author: Richard Hogge
+Author URI: https://richardhogge.github.io/
+*/
