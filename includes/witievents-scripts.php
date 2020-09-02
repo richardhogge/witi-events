ef97e1e7c9ddb5bad012fed4d90aa040d7ba7df0
Richard Hogge <richard@tremolocreative.com>
2020-09-01 22:54:36 -0700

    refactor naming convention

--- includes/witievents-scripts.php
+++ includes/witievents-scripts.php
@@ -1,8 +1,8 @@
 <?php
 
-// CSS + JS
-function we_add_scripts() {
+// Enqueue scripts and styles
+function witi_scripts() {
   wp_enqueue_style('we-main-style', plugins_url() . '/witievents/css/style.css');
   wp_enqueue_script('we-main-script', plugins_url() . '/witievents/js/main.js');
 }
-add_action('wp_enqueue_scripts', 'we_add_scripts');
+add_action('wp_enqueue_scripts', 'witi_scripts');
